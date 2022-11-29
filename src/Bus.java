0d354bcd2e7e4e32114b0ace485147164943c6d9
Drfox81 <Drfox11181@mail.ru>
2022-11-29 21:12:30 +0300

    Посмотрите с джинериками здесь не особо понял, если есть возможность в двух словах, либо пример в моей программе

---
+++ src/Bus.java
@@ -0,0 +1,26 @@
+public class Bus extends Transport implements Competing {
+
+    public Bus(String brand, String model, String fuel) {
+        super(brand, model, fuel);
+    }
+
+    public Bus(String brand, String model, String fuel, double engineCapacity) {
+        super(brand, model, fuel, engineCapacity);
+    }
+
+    @Override
+    public void pitStop() {
+        System.out.println("Провести pit-stop \n" + "pit-stop - проведен!");
+    }
+
+    @Override
+    public void bestTime() {
+        System.out.println(getBrand() + " " + getModel() + " - показал лучшее время на этом круге!!!");
+    }
+
+    @Override
+    public void maxSpeed() {
+        System.out.println("достиг максимальной скорости 150 км/ч");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Car ladaVesta = new Car("Lada", "Vesta", "бензин");
+//        Car ladaKalina = new Car("Lada","Kalina",1.6);
+//        Car bmw5 = new Car("BMW","X5","бензин");
+////        Car kiaRio = new Car("Kia","Rio",1.6);
+////
+//        Trucks kamaz = new Trucks("Kamaz", "3008","дизель");
+////
+        Bus paz = new Bus("Паз", "2645","дизель");
+////
+//        paz.start();
+//        paz.stop();
+//        DriverB<Car> driverB = new DriverB<>("Иванов Иван Иванович", 4);
+//        driverB.refill(paz,20);
+//        driverB.getDriverAuto(ladaVesta);
+//        DriverC driverC= new DriverC<>("Петров Петр Петрович",5);
+//        driverC.getDriverAuto(kamaz);
+//        System.out.println(driverC.getDriverLicense());
+        DriverB<Car> sergeevB =new DriverB<>("Сергеев Сергей Сергеевич",4);
+        System.out.println(sergeevB.getDriverLicense());
+        sergeevB.getDriverAuto(ladaVesta);
+        sergeevB.start();
+        System.out.println(sergeevB.getDriverLicense());
+
+
+
+//        driverC.getDriverAuto(kamaz);
+//        paz.pitStop();
+//        paz.bestTime();
+//        paz.maxSpeed();
+//        bmw5.bestTime();
+//
+
+
     }
 }
